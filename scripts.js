ffd295a8f23d35f76de2ee341d76869b74bd8b24
Candela Martinez <mcandelamartinez76@gmail.com>
2022-02-04 14:18:43 +0100

    First Commit

---
+++ scripts.js
@@ -0,0 +1,34 @@
+/*const title= document.getElementById('title')
+
+//title.textContent= 'DOM - accediendo a nodos'
+
+const paragraph = document.querySelector('.paragraph')
+
+const span= paragraph.querySelector("span")
+
+console.log(span.textContent)
+
+const span2= document.getElementById('title').querySelector('span')
+console.log(span2.textContent)
+
+const paragraphs= document.querySelectorAll('.paragraph')
+console.log(paragraphs)
+
+const paragraphSpread= [...document.querySelectorAll('.paragraph')]
+
+
+
+const paragraphArray= Array.from(document.querySelectorAll('.paragraph'))
+
+paragraphArray.map(p=>console.log(p.children))*/
+
+const title= document.getElementById('title')
+const names= document.getElementById('name')
+
+//console.log(names.getAttribute('type'))
+
+//names.setAttribute('type','date')
+//console.log(names)
+
+console.log(title)
+console.log(names)
